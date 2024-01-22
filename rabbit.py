b5adf87a7564d4fa1a33b6fe2c5f570988424081
karasu <gustaucastells@gmail.com>
2024-01-22 10:13:02 +0100

    Small changes

--- rabbit.py
+++ rabbit.py
@@ -1,30 +1,44 @@
-""" Send messages to rabbitmq """
+""" Send messages to rabbitmq (producer)"""
 
 import json
 
 import pika
 
+QUEUE = 'switchbox'
+EXCHANGE = 'switchbox'
+EXCHANGE_TYPE = 'direct'
+
 def connect():
     """ Connect to rabbitmq """
-    connection =  pika.BlockingConnection(
-        pika.ConnectionParameters(
+    credentials = pika.PlainCredentials('guest', 'guest')
+
+    parameters = pika.ConnectionParameters(
             host='localhost',
             port=5672,
             virtual_host='/',
-            credentials=pika.PlainCredentials('guest', 'guest')
-        )
+            credentials=credentials
     )
-    return connection
+
+    return pika.BlockingConnection(parameters)
 
 def send(profile):
     """ Sends a message to rabbitmq """
 
     connection = connect()
+
     channel = connection.channel()
-    channel.queue_declare(queue="switchbox")
+
+    channel.exchange_declare(
+            exchange=EXCHANGE,
+            exchange_type=EXCHANGE_TYPE,
+            passive=False,
+            durable=True,
+            auto_delete=False)
+
+    channel.queue_declare(queue=QUEUE)
 
     channel.basic_publish(
-        exchange='',
+        exchange=EXCHANGE,
         routing_key='switchbox',
         body=json.dumps(profile))
 
